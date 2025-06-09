61babb19ef8a9d23b8b5a15c8ee5655c26f1a135
ashishd881 <ashishkpdh881@gmail.com>
2025-06-09 16:02:14 +0530

    Fix import case sensitivity for Description and Footer components in Home.jsx

--- Imagify/client/src/pages/Home.jsx
+++ Imagify/client/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
-import Description from '../components/description'
+import Description from '../components/Description'
 import Testimonials from '../components/Testimonials'
 import GenerateBtn from '../components/GenerateBtn'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 
 function Home() {
   return (
